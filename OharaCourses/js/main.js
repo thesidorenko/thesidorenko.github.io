82f24e6eb8d69589a2820158699b06789ba0423e
thesidorenko <sidorennko21@gmail.com>
2020-09-11 01:43:57 +0300

    edited project

--- OharaCourses/js/main.js
+++ OharaCourses/js/main.js
@@ -1,7 +1,7 @@
 (function($) {
 $(function() {
   
-  $('ul .tabs_caption').on('click', 'li:not(.active)', function() {
+  $('ul.tabs_caption').on('click', 'li:not(.active)', function() {
     $(this)
       .addClass('active').siblings().removeClass('active')
       .closest('div.tabs').find('div.tabs_content').removeClass('active').eq($(this).index()).addClass('active');
